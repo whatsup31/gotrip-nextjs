964eb890f11f05b062f9c901419f73d44ac1a6cf
AIC31 <ichalhane@gmail.com>
2025-09-17 16:44:25 +0200

    merge dashboards (c, v, p) with backend

--- app/(dashboard)/conciergerie-dashboard/add-hotel/page.jsx
+++ app/(dashboard)/conciergerie-dashboard/add-hotel/page.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import DashboardPage from "../../../../components/dashboard/partenaire-dashboard/add-property";
+import DashboardPage from "../../../../components/dashboardc/conciergerie-dashboard/add-hotel";
+
+export const metadata = {
+  title: "Vendor Add Hotel || GoTrip - Travel & Tour React NextJS Template",
+  description: "GoTrip - Travel & Tour React NextJS Template",
+};
 
 export default function page() {
   return (
--- app/(dashboard)/conciergerie-dashboard/dashboard/page.jsx
+++ app/(dashboard)/conciergerie-dashboard/dashboard/page.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import DashboardPage from "../../../../components/dashboard/partenaire-dashboard/dashboard";
+import DashboardPage from "../../../../components/dashboardc/conciergerie-dashboard/dashboard";
+
+export const metadata = {
+  title: "Vendor Dashboard || GoTrip - Travel & Tour React NextJS Template",
+  description: "GoTrip - Travel & Tour React NextJS Template",
+};
 
 export default function page() {
   return (
