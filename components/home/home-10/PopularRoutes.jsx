8d1c4557699c2735e8b1a732859f51fcdda3cb49
whatsup31 <whatsupomi@gmail.com>
2025-10-02 12:58:59 +0200

    Update PopularRoutes.jsx

--- components/home/home-10/PopularRoutes.jsx
+++ components/home/home-10/PopularRoutes.jsx
@@ -3,29 +3,40 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation } from "swiper/modules";
+import { Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+// util du thème pour comparer les tags (optionnel mais identique à Hotels)
+import isTextMatched from "@/utils/isTextMatched";
 
 const PopularRoutes = () => {
+  // ⚠️ CONTENU INCHANGÉ
   const rentalRoutes = [
-    { id: 1, img: "/img/rentals/1.png", destination: "Istanbul - New York", tripType: "Round-trip", date: "Wed, Jun 1 - Sun, Jun 5", price: "72", delayAnimation: "100" },
-    { id: 2, img: "/img/rentals/2.png", destination: "Istanbul - Paris", tripType: "Round-trip", date: "Wed, Jun 1 - Sun, Jun 5", price: "72", delayAnimation: "200" },
-    { id: 3, img: "/img/rentals/3.png", destination: "Istanbul - Antalya", tripType: "Round-trip", date: "Wed, Jun 1 - Sun, Jun 5", price: "72", delayAnimation: "300" },
-    { id: 4, img: "/img/rentals/4.png", destination: "Istanbul - London", tripType: "Round-trip", date: "Wed, Jun 1 - Sun, Jun 5", price: "72", delayAnimation: "400" },
-    { id: 5, img: "/img/rentals/1.png", destination: "Istanbul - New York", tripType: "Round-trip", date: "Wed, Jun 1 - Sun, Jun 5", price: "72", delayAnimation: "500" },
-    { id: 6, img: "/img/rentals/2.png", destination: "Istanbul - Paris", tripType: "Round-trip", date: "Wed, Jun 1 - Sun, Jun 5", price: "72", delayAnimation: "600" },
+    { id: 1, img: "/img/rentals/1.png", destination: "Istanbul - New York", tripType: "Round-trip", date: "Wed, Jun 1 - Sun, Jun 5", price: "72", delayAnimation: "100", tag: "" },
+    { id: 2, img: "/img/rentals/2.png", destination: "Istanbul - Paris",   tripType: "Round-trip", date: "Wed, Jun 1 - Sun, Jun 5", price: "72", delayAnimation: "200", tag: "" },
+    { id: 3, img: "/img/rentals/3.png", destination: "Istanbul - Antalya", tripType: "Round-trip", date: "Wed, Jun 1 - Sun, Jun 5", price: "72", delayAnimation: "300", tag: "" },
+    { id: 4, img: "/img/rentals/4.png", destination: "Istanbul - London",  tripType: "Round-trip", date: "Wed, Jun 1 - Sun, Jun 5", price: "72", delayAnimation: "400", tag: "" },
+    { id: 5, img: "/img/rentals/1.png", destination: "Istanbul - New York", tripType: "Round-trip", date: "Wed, Jun 1 - Sun, Jun 5", price: "72", delayAnimation: "500", tag: "" },
+    { id: 6, img: "/img/rentals/2.png", destination: "Istanbul - Paris",   tripType: "Round-trip", date: "Wed, Jun 1 - Sun, Jun 5", price: "72", delayAnimation: "600", tag: "" },
   ];
 
   return (
     <>
       <Swiper
-        modules={[Navigation]}
         spaceBetween={30}
-        loop
-        navigation={{ nextEl: ".js-routes_next", prevEl: ".js-routes_prev" }}
+        modules={[Navigation, Pagination]}
+        navigation={{
+          nextEl: ".js-routes-next",
+          prevEl: ".js-routes-prev",
+        }}
+        pagination={{
+          el: ".js-routes-pag",
+          clickable: true,
+        }}
         breakpoints={{
-          500: { slidesPerView: 2, spaceBetween: 20 },
+          540: { slidesPerView: 2, spaceBetween: 20 },
           768: { slidesPerView: 2, spaceBetween: 22 },
           1024: { slidesPerView: 3 },
           1200: { slidesPerView: 4 },
@@ -33,61 +44,114 @@ const PopularRoutes = () => {
       >
         {rentalRoutes.map((item) => (
           <SwiperSlide key={item.id}>
-            {/* Carte cliquable + bouton en bas à droite */}
-            <div className="rentalCard -type-2" data-aos="fade" data-aos-delay={item.delayAnimation} style={{ position: "relative" }}>
-              {/* Image cliquable */}
-              <Link href="/rental-list-v2" className="rentalCard__image">
-                <div className="cardImage ratio ratio-6:5">
+            {/* même structure/skins que Hotels */}
+            <Link
+              href="/rental-list-v2"
+              className="hotelsCard -type-1 hover-inside-slider"
+              data-aos="fade"
+              data-aos-delay={item.delayAnimation}
+            >
+              <div className="hotelsCard__image">
+                <div className="cardImage ratio ratio-1:1">
                   <div className="cardImage__content">
-                    <Image width={300} height={250} className="rounded-4 col-12" src={item.img} alt={item.destination} />
+                    {/* slider interne dans la vignette (comme Hotels) */}
+                    <div className="cardImage-slider rounded-4 overflow-hidden custom_inside-slider">
+                      <Swiper
+                        className="mySwiper"
+                        modules={[Pagination, Navigation]}
+                        pagination={{ clickable: true }}
+                        navigation
+                      >
+                        {/* ton contenu ne fournit qu’une image => on la duplique pour le slider interne */}
+                        {[item.img, item.img, item.img].map((slide, i) => (
+                          <SwiperSlide key={i}>
+                            <Image
+                              width={300}
+                              height={300}
+                              className="rounded-4 col-12 js-lazy"
+                              src={slide}
+                              alt={item.destination}
+                            />
+                          </SwiperSlide>
+                        ))}
+                      </Swiper>
+                    </div>
                   </div>
                 </div>
-              </Link>
 
-              {/* Contenu cliquable (titre) */}
-              <div className="rentalCard__content mt-10">
-                <h4 className="rentalCard__title text-dark-1 text-18 lh-16 fw-500">
-                  <Link href="/rental-list-v2">
-                    <span>{item.destination}</span>
-                  </Link>
+                {/* bouton wishlist (identique) */}
+                <div className="cardImage__wishlist">
+                  <button className="button -blue-1 bg-white size-30 rounded-full shadow-2">
+                    <i className="icon-heart text-12" />
+                  </button>
+                </div>
+
+                {/* badge gauche (identique à Hotels, s’affiche si `tag` non vide) */}
+                {item.tag ? (
+                  <div className="cardImage__leftBadge">
+                    <div
+                      className={`py-5 px-15 rounded-right-4 text-12 lh-16 fw-500 uppercase
+                        ${isTextMatched(item.tag, "breakfast included") ? "bg-dark-1 text-white" : ""}
+                        ${isTextMatched(item.tag, "best seller") ? "bg-blue-1 text-white" : ""}
+                        ${isTextMatched(item.tag, "-25% today") ? "bg-brown-1 text-white" : ""}
+                        ${isTextMatched(item.tag, "top rated") ? "bg-yellow-1 text-dark-1" : ""}`
+                      }
+                    >
+                      {item.tag}
+                    </div>
+                  </div>
+                ) : null}
+              </div>
+
+              {/* contenu identique, mappé sur tes champs */}
+              <div className="hotelsCard__content mt-10">
+                <h4 className="hotelsCard__title text-dark-1 text-18 lh-16 fw-500">
+                  <span>{item.destination}</span>
                 </h4>
 
-                <div className="d-flex items-center text-light-1">
-                  <div className="text-14">{item.tripType}</div>
-                  <div className="size-3 bg-light-1 rounded-full ml-10 mr-10" />
-                  <div className="text-14">{item.date}</div>
+                <p className="text-light-1 lh-14 text-14 mt-5">
+                  {item.tripType}
+                  <span className="size-3 bg-light-1 rounded-full mx-10 inline-block" />
+                  {item.date}
+                </p>
+
+                <div className="d-flex items-center mt-20">
+                  <div className="flex-center bg-blue-1 rounded-4 size-30 text-12 fw-600 text-white">
+                    4.8
+                  </div>
+                  <div className="text-14 text-dark-1 fw-500 ml-10">Exceptional</div>
+                  <div className="text-14 text-light-1 ml-10">2345 reviews</div>
                 </div>
 
-                {/* Footer prix + bouton */}
-                <div className="d-flex items-center justify-between mt-10">
-                  <div className="text-light-1">
-                    <span className="fw-500 text-dark-1">US${item.price}</span>
-                    <span> / per night</span>
+                <div className="mt-5">
+                  <div className="fw-500">
+                    Starting from <span className="text-blue-1">US${item.price}</span>
                   </div>
-
-                  {/* Bouton en bas à droite, même style que “Réserver” */}
-                  <Link
-                    href="/rental-list-v2"
-                    className="button -md -dark-1 bg-blue-1 text-white"
-                    style={{ whiteSpace: "nowrap" }}
-                  >
-                    Ajouter
-                    <i className="icon-plus ml-10" />
-                  </Link>
                 </div>
               </div>
-            </div>
+            </Link>
           </SwiperSlide>
         ))}
       </Swiper>
 
-      {/* flèches */}
-      <button className="section-slider-nav -prev flex-center button -blue-1 bg-white text-dark-1 size-40 rounded-full shadow-1 sm:d-none js-routes_prev">
-        <i className="icon icon-chevron-left text-12" />
-      </button>
-      <button className="section-slider-nav -next flex-center button -blue-1 bg-white text-dark-1 size-40 rounded-full shadow-1 sm:d-none js-routes_next">
-        <i className="icon icon-chevron-right text-12" />
-      </button>
+      {/* navigation & pagination identiques au composant Hotels */}
+      <div className="d-flex x-gap-15 items-center justify-center sm:justify-start pt-40 sm:pt-20">
+        <div className="col-auto">
+          <button className="d-flex items-center text-24 arrow-left-hover js-routes-prev">
+            <i className="icon icon-arrow-left" />
+          </button>
+        </div>
+
+        <div className="col-auto">
+          <div className="pagination -dots text-border js-routes-pag" />
+        </div>
+
+        <div className="col-auto">
+          <button className="d-flex items-center text-24 arrow-right-hover js-routes-next">
+            <i className="icon icon-arrow-right" />
+          </button>
+        </div>
+      </div>
     </>
   );
 };
